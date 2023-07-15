641a36b2080d893c37b2c5e23799dea714c5ff91
Jingbo(Bobo) Liu <719744328ccs@gmail.com>
2023-07-16 02:42:33 +0800

    Delete most of the demos in Feels

---
+++ TROM/Assets/Test/Bobo/PlayerController/PlayerController.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PlayerControllerTest
+{
+    public class PlayerController : MonoBehaviour
+    {
+
+    }
+}
---
+++ TROM/Assets/_Scripts/Player.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Where we store all information and data of player
+public class Player
+{
+    public int Capacity; // How much weight player can hold. 
+
+    public float Speed; // How fast player can move. 
+    
+    
+}
