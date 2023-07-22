67a6f8db1d0dbac9ee4f4f1f9ca45d155c18ace2
郑倍添Tim <kentcheng105@gmail.com>
2023-07-22 20:41:35 +0800

    Testing behavior designer and way point

---
+++ TROM/Assets/_Scripts/CustomBehaviorTreeComponents/CanSeeObjectFlipX.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using BehaviorDesigner.Runtime;
+using BehaviorDesigner.Runtime.Tasks;
+using BehaviorDesigner.Runtime.Tasks.Movement;
+
+[TaskDescription("Check to see if the any objects are within sight of the agent.")]
+[TaskCategory("Movement")]
+[TaskIcon("Assets/Behavior Designer Movement/Editor/Icons/{SkinColor}CanSeeObjectIcon.png")]
+public class CanSeeObjectFlipX : CanSeeObject
+{
+    public bool spriteFaceRight = true;
+    public SpriteRenderer entitySpriteRenderer;
+    public float angleOffset;
+    
+    public override void OnAwake()
+    {
+        var entity = transform.GetComponent<GameEntity>();
+        if (entity == null) return;
+        entitySpriteRenderer = entity.spriteRenderer;
+    }
+    
+    public override TaskStatus OnUpdate()
+    {
+        if (entitySpriteRenderer == null) return TaskStatus.Failure;
+            
+        if (spriteFaceRight)
+        {
+            //sprite initially facing right, if flipped X, should be negative offset
+            if(entitySpriteRenderer.flipX) angleOffset2D.Value = -angleOffset;
+            else angleOffset2D.Value = angleOffset;
+        }
+        else
+        {
+            //sprite initially facing left, if flipped X, should be positive offset
+            if(entitySpriteRenderer.flipX) angleOffset2D.Value = angleOffset;
+            else angleOffset2D.Value = -angleOffset;
+        }
+        return base.OnUpdate();
+    }
+}
